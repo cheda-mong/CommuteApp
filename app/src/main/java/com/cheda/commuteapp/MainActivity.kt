bd38e25b24f897cf02e4ed7b55115c6f0d3766c5
cheda-mong <leejh0651@gmail.com>
2025-05-14 14:40:26 +0900

    Create MainActivity.kt

---
+++ app/src/main/java/com/cheda/commuteapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cheda.commuteapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
